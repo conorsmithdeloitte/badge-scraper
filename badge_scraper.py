b15e8ca0413d4703e6985fefc0f8e2ec8b68c1a7
Smith <conorsmith@deloitte.com>
2022-12-08 14:00:59 -0500

    code cleanup

--- badge_scraper.py
+++ badge_scraper.py
@@ -15,56 +15,6 @@ from selenium.common.exceptions import TimeoutException
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-# #SECTION: Variable declaration
-# username = 'eszmigielski'
-# #username = 'csmith288'
-# url = 'https://trailblazer.me/id/' + username
-
-# #SECTION: Find HTML element that contains badge / point data
-# driver = webdriver.Chrome()
-# driver.get(url)
-# shadow_host = driver.find_element(By.CSS_SELECTOR, '#profile-sections-container')
-# shadow_root = shadow_host.shadow_root
-# shadow_content = shadow_root.find_element(By.CLASS_NAME, 'root')
-
-# #NOTE: The goal of the below test code is to get to an element further down in the hierarchy so I don't have to string parse for the badge
-# #test_root = shadow_content.shadow_root
-# #badge = test_root.find_element(By.CLASS_NAME, 'card__header-title')
-
-# #NOTE: The below print statement shows what the full root string looks like
-# #print(shadow_content.text)
-
-# #SECTION: Parse raw result to get the badge / point values
-# total_new_lines = 0
-# badge_and_point_new_line_location = 0
-# number_of_badges = ''
-# number_of_points = ''
-# for x in shadow_content.text:
-#     if x == '\n':
-#         total_new_lines += 1
-# for x in shadow_content.text:
-#     if x == '\n':
-#         badge_and_point_new_line_location += 1
-#     elif badge_and_point_new_line_location == total_new_lines - 6:
-#         number_of_badges += x
-#     elif badge_and_point_new_line_location == total_new_lines - 4:
-#         number_of_points += x
-
-# #SECTION: Print final result
-# print('Number of Badges:', number_of_badges)
-# print('Number of Points:', number_of_points)
-
-# # NOTE: Don't need below lines since this doesn't work for parsing shadow dom
-# # page = requests.get(URL, verify=False)
-# # soup = BeautifulSoup(page.content, 'html.parser')
-# # print(soup.prettify())
-# # print(soup.title)
-# # s = soup.find('div', class_='card__header-title')
-# # s = soup.find('div', class_='card__header-title')
-# # print(s.find_all('h2'))
-# # content = s.final_all('p')
-# # print(content)
-
 @app.route('/trailblazer/', methods=['GET'])
 def respond():
     # Retrieve the name from the url parameter /trailblazer/?username=
@@ -95,30 +45,10 @@ def respond():
         except TimeoutException:
             print("Timeout error, took too long to load")
 
-        #NOTE: The goal of the below test code is to get to an element further down in the hierarchy so I don't have to string parse for the badge
-        #test_root = shadow_content.shadow_root
-        #badge = test_root.find_element(By.CLASS_NAME, 'card__header-title')
-
         #NOTE: The below print statement shows what the full root string looks like
         print(shadow_content.text)
 
         #SECTION: Parse raw result to get the badge / point values
-        # total_new_lines = 0
-        # badge_and_point_new_line_location = 0
-        # number_of_badges = ''
-        # number_of_points = ''
-        # for x in shadow_content.text:
-        #     if x == '\n':
-        #         total_new_lines += 1
-        # for x in shadow_content.text:
-        #     if x == '\n':
-        #         badge_and_point_new_line_location += 1
-        #     elif badge_and_point_new_line_location == total_new_lines - 6:
-        #         number_of_badges += x
-        #     elif badge_and_point_new_line_location == total_new_lines - 4:
-        #         number_of_points += x
-        
-        #UPDATED CODE TO MAKE IT MORE DYNAMIC
         l = []
         item = ''
         for x in shadow_content.text:
@@ -141,21 +71,20 @@ def respond():
             
             
 
-        #SECTION: Print final result
+        #SECTION: Save & print final result
         response["Badges"] = number_of_badges
         response["Points"] = number_of_points
-        #response["MESSAGE"] = f"Number of Badges Test: {username}"
 
         print('Number of Badges:', number_of_badges)
         print('Number of Points:', number_of_points)
 
-    # Return the response in json format
+    # Return the response in JSON format
     return jsonify(response)
 
 @app.route('/')
 def index():
     # A welcome message to test our server
-    return "<h1>Welcome to the badge scraper API!</h1>"
+    return "<h2>Welcome to the badge scraper API!</h2>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
