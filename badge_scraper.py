8b0f1b9407a79284902ee067f1f166cd1ce62e51
Smith <conorsmith@deloitte.com>
2023-12-14 15:37:37 -0500

    fixed chrome connection

--- badge_scraper.py
+++ badge_scraper.py
@@ -45,7 +45,6 @@ def respond():
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-        driver = webdriver.Chrome() 
         driver.get(url)
         delay = 10 #seconds
 
